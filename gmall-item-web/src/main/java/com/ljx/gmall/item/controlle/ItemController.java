7e055ecbc14bc9d1d3a7f3c00d02881994888d2f
lijinxing <lijinx-pccw@139.com>
2020-02-10 20:15:45 +0800

    更新商品详情控制层

--- gmall-item-web/src/main/java/com/ljx/gmall/item/controlle/ItemController.java
+++ gmall-item-web/src/main/java/com/ljx/gmall/item/controlle/ItemController.java
@@ -1,8 +1,10 @@
 package com.ljx.gmall.item.controlle;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
 import com.ljx.gmall.bean.PmsProductSaleAttr;
 import com.ljx.gmall.bean.PmsSkuInfo;
+import com.ljx.gmall.bean.PmsSkuSaleAttrValue;
 import com.ljx.gmall.service.SkuService;
 import com.ljx.gmall.service.SpuService;
 import org.springframework.stereotype.Controller;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @CrossOrigin
@@ -36,6 +40,20 @@ public class ItemController {
         //返回销售属性列表
         List<PmsProductSaleAttr> saleAttrList = spuService.spuSaleAttrListCheckBySku(skuInfo.getProductId(),skuInfo.getId());
         map.put("spuSaleAttrListCheckBySku",saleAttrList);
+        //查询当前sku相关的sku信息的hash表
+        Map<String,String> skuSaleValueHash = new HashMap<>();
+        List<PmsSkuInfo> skuInfos =  skuService.getSkuSaleAttrValueListBySpu(skuInfo.getProductId());
+        for (PmsSkuInfo pmsSkuInfo : skuInfos) {
+            String k = "";
+            String v = pmsSkuInfo.getId();
+            List<PmsSkuSaleAttrValue> skuSaleAttrValues = pmsSkuInfo.getSkuSaleAttrValueList();
+            for (PmsSkuSaleAttrValue saleAttrValue : skuSaleAttrValues) {
+                k+=saleAttrValue.getSaleAttrValueId()+"|";
+            }
+            skuSaleValueHash.put(k,v);
+        }
+        String jsonString = JSON.toJSONString(skuSaleValueHash);
+        map.put("skuSaleValueHash",jsonString);
         return "item";
     }
 }
