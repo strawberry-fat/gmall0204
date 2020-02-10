81c03a18d421d21ded25d6700e9de0671d52e09b
lijinxing <lijinx-pccw@139.com>
2020-02-10 20:14:59 +0800

    更新skumapper

--- gmall-manage-service/src/main/java/com/ljx/gmall/manage/mapper/PmsSkuInfoMapper.java
+++ gmall-manage-service/src/main/java/com/ljx/gmall/manage/mapper/PmsSkuInfoMapper.java
@@ -2,6 +2,10 @@ package com.ljx.gmall.manage.mapper;
 
 import com.ljx.gmall.bean.PmsSkuInfo;
 import com.ljx.gmall.manage.BaseMapper.TkBaseMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface PmsSkuInfoMapper extends TkBaseMapper<PmsSkuInfo> {
+    List<PmsSkuInfo> selectSkuSaleAttrValueListBySpu(@Param("productId") String productId);
 }
