9e4c8fbb5c07658f18603c9b709ff5e6ff2b9c22
lijinxing <lijinx-pccw@139.com>
2020-02-22 21:58:20 +0800

    搜索用的方法

--- gmall-api/src/main/java/com/ljx/gmall/service/SkuService.java
+++ gmall-api/src/main/java/com/ljx/gmall/service/SkuService.java
@@ -2,8 +2,15 @@ package com.ljx.gmall.service;
 
 import com.ljx.gmall.bean.PmsSkuInfo;
 
+import java.util.List;
+
 public interface SkuService {
     void saveSkuInfo(PmsSkuInfo pmsSkuInfo);
 
     PmsSkuInfo getSkuById(String skuId);
+    /*
+        查询当前sku相关的sku信息
+     */
+    List<PmsSkuInfo> getSkuSaleAttrValueListBySpu(String productId);
+    List<PmsSkuInfo> getAllSku(String catalog3Id);
 }
