7c0124e9dc745f20f817c906c1afef706d4969d0
lijinxing <lijinx-pccw@139.com>
2020-03-04 20:08:04 +0800

    Redis工具类更新

--- gmall-service-util/src/main/java/com/ljx/gmall/util/RedisUtil.java
+++ gmall-service-util/src/main/java/com/ljx/gmall/util/RedisUtil.java
@@ -1,5 +1,7 @@
 package com.ljx.gmall.util;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
@@ -7,19 +9,38 @@ import redis.clients.jedis.JedisPoolConfig;
 public class RedisUtil {
 
     private JedisPool jedisPool;
+    private static Log logger = LogFactory.getLog(RedisUtil.class);
 
-    public void initPool(String host,int port ,int database){
+    public void initPool(String host, int port, int database) {
         JedisPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setMaxTotal(200);
-        poolConfig.setMaxIdle(30);
+        //最大连接数
+        poolConfig.setMaxTotal(50);
+        //最小空闲连接数
+        poolConfig.setMaxIdle(10);
         poolConfig.setBlockWhenExhausted(true);
-        poolConfig.setMaxWaitMillis(10*1000);
+        poolConfig.setMaxWaitMillis(10 * 1000);
         poolConfig.setTestOnBorrow(true);
-        jedisPool=new JedisPool(poolConfig,host,port,20*1000);
+        poolConfig.setTestWhileIdle(true);
+        poolConfig.setNumTestsPerEvictionRun(10);
+        jedisPool = new JedisPool(poolConfig, host, port, 20 * 1000);
     }
 
-    public Jedis getJedis(){
-        Jedis jedis = jedisPool.getResource();
+    /**
+     * 获取Jedis对象
+     *
+     * @return
+     */
+    public synchronized Jedis getJedis() {
+        Jedis jedis = null;
+        if (jedisPool != null) {
+            try {
+                if (jedis == null) {
+                    jedis = jedisPool.getResource();
+                }
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+        }
         return jedis;
     }
 
